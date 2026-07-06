bada189b69bc9f78d7ea1a592b805c37d7326d55
lifechitect-max <lifechitect@gmail.com>
2026-07-06 11:25:56 -0400

    v7: one-tap save to gallery after each photo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'inspect-v6';
+const CACHE = 'inspect-v7';
 const ASSETS = ['./', 'index.html', 'manifest.webmanifest', 'icon-180.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
