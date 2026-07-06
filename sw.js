ca8811ad6fccddfd5627328b83a7cbc66de0b3e5
lifechitect-max <lifechitect@gmail.com>
2026-07-06 11:07:45 -0400

    Fix stuck Starting camera overlay

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'inspect-v3';
+const CACHE = 'inspect-v5';
 const ASSETS = ['./', 'index.html', 'manifest.webmanifest', 'icon-180.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
