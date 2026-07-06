a201443e2c2351deae0d777854b08a9aba68aca9
lifechitect-max <lifechitect@gmail.com>
2026-07-06 10:31:49 -0400

    Native-style camera: zoom chips, 0.5x via multi-lens camera, no interstitial

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'inspect-v2';
+const CACHE = 'inspect-v3';
 const ASSETS = ['./', 'index.html', 'manifest.webmanifest', 'icon-180.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
