f0424942a0f8892a54d08547f283d87f7f1fa4f2
lifechitect-max <lifechitect@gmail.com>
2026-07-06 14:31:35 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'inspect-v8';
+const CACHE = 'inspect-v10';
 const ASSETS = ['./', 'index.html', 'manifest.webmanifest', 'icon-180.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
